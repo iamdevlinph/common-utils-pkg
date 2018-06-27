0caf049f240c76879b0ec4597d42a169ae4cfe99
iamdevlinph <iamdevlinph@gmail.com>
2018-06-27 11:56:34 +0800

    color.randomPastelColor() test

--- src/color/color.util.spec.js
+++ src/color/color.util.spec.js
@@ -1,7 +1,7 @@
 /* global describe, it */
 
 import chai from 'chai';
-import Color from './color.util';
+import ColorUtil from './color.util';
 
 chai.expect();
 
@@ -12,7 +12,13 @@ const {
 describe('COLOR', () => {
   describe('when i run test', () => {
     it('should "Color OK"', () => {
-      expect(Color.test()).to.be.equal('Color OK');
+      expect(ColorUtil.test()).to.be.equal('Color OK');
+    });
+  });
+
+  describe('randomPastelColor', () => {
+    it('should return random HSL color', () => {
+      expect(ColorUtil.randomPastelColor()).to.include('hsl');
     });
   });
 });
