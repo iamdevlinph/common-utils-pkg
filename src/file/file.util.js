19981eeaf8a46e78da685d71c63c2cafcc46c675
iamdevlinph <iamdevlinph@gmail.com>
2018-07-03 18:20:11 +0800

    Breakdown FileUtil

--- src/file/file.util.js
+++ src/file/file.util.js
@@ -1,15 +1,6 @@
 import CoreUtil from '../core/core.util';
 
-const isWholeNum = number => number % 1 === 0;
-
-const convertBytes = (bytes, decimals) => {
-  const units = ['byte(s)', 'kB', 'MB', 'GB', 'TB', 'PB'];
-  const number = Math.floor(Math.log(bytes) / Math.log(1024));
-  const converted = bytes / (1024 ** Math.floor(number));
-  const unit = units[number];
-  const formatted = isWholeNum(converted) || !decimals ? converted : converted.toFixed(decimals);
-  return `${formatted} ${unit}`;
-};
+import convertBytes from './utils/convertBytes';
 
 const FileUtil = {
   convertBytes: CoreUtil.takes(convertBytes, 'number')
