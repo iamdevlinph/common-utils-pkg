e99e118a20a491c64a977fec9b1d23edf2a412b0
iamdevlinph <iamdevlinph@gmail.com>
2018-11-04 22:48:21 +0800

    Create ObjectUtil.isEmpty method

--- src/object/object.util.js
+++ src/object/object.util.js
@@ -1,3 +1,9 @@
-const ObjectUtil = {};
+import CoreUtil from '../core/core.util';
+
+import isEmpty from './methods/isEmpty';
+
+const ObjectUtil = {
+  isEmpty: CoreUtil.takes(isEmpty, 'object')
+};
 
 export default ObjectUtil;
