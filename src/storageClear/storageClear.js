ac6bba781fa4a0a4e63bc8802cadd5eddb13ae49
iamdevlinph <iamdevlinph@gmail.com>
2019-05-15 15:26:43 +0800

    Storage | clear -> storageClear

---
+++ src/storageClear/storageClear.js
@@ -0,0 +1,13 @@
+/**
+ * Removes all data stored in the local storage.
+ *
+ * @module Storage
+ * @name storageClear
+ * @example
+ *
+ * storageClear();
+ *
+ */
+const storageClear = () => window.localStorage.clear();
+
+export default storageClear;
