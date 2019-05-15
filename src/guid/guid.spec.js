9d902a5ea697619f6eb39a0df7963c74e540818e
iamdevlinph <iamdevlinph@gmail.com>
2019-05-15 12:19:39 +0800

    Key | guid

---
+++ src/guid/guid.spec.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+
+import chai from 'chai';
+import guid from './guid';
+
+chai.expect();
+
+const { expect } = chai;
+
+describe('guid', () => {
+  it('should generate a guid', () => {
+    expect(guid()).to.be.a('string');
+  });
+});
