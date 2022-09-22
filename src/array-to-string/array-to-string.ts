31d7ababf60dde0d683bd101c09c3bfaeaf107ba
Devlin Pajaron <iamdevlinph@gmail.com>
2022-09-22 23:19:07 +0800

    [TS] array-to-string

---
+++ src/array-to-string/array-to-string.ts
@@ -0,0 +1,17 @@
+/**
+ * Convert an array to a string and combine on the delimiter passed.
+ *
+ * @module String
+ * @name arrayToString
+ * @param array array to convert to string
+ * @param delimiter  characater to use in combining to string
+ * @returns Returns a string from the array.
+ *
+ * @example
+ *
+ * arrayToString(['The', 'quick', 'brown'], '-');
+ * // => 'The-brown-fox'
+ */
+
+export const arrayToString = (array: string[] | number[], delimiter: string) =>
+  array.join(delimiter);
