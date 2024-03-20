5c5208cd0efaeec47dfd9ec50213f4a0e51c0311
Devlin Pajaron <iamdevlinph@gmail.com>
2024-03-20 20:16:20 +0800

    WIP - docusaurus

--- scripts/md-generate.mjs
+++ scripts/md-generate.mjs
@@ -2,7 +2,7 @@ import * as documentation from 'documentation';
 import fs from 'fs';
 import { readdir } from 'fs/promises';
 
-const OUTPUT_DIR = 'docusaurus/';
+const OUTPUT_DIR = 'docs-md/api/';
 
 /**
  * Delete OUTPUT_DIR before building
@@ -10,6 +10,18 @@ const OUTPUT_DIR = 'docusaurus/';
  */
 fs.rmSync(OUTPUT_DIR, { recursive: true, force: true });
 fs.mkdirSync(OUTPUT_DIR);
+// generate the _category_.json for docusaurus again
+fs.writeFileSync(
+  `${OUTPUT_DIR}/_category_.json`,
+  JSON.stringify({
+    label: 'API',
+    position: 2,
+    link: {
+      type: 'generated-index',
+      description: 'API reference for available methods.',
+    },
+  })
+);
 
 /**
  * Get all method paths from src/
@@ -26,6 +38,7 @@ const methodDirectory = await getDirectories('src/');
  * Loop each directory to generate markdown
  */
 methodDirectory.forEach(async (directory) => {
+  // const directory = 'src/array-remove-item/array-remove-item.ts';
   /**
    * Get method info by getting as JSON first
    * since i don't know how to access this when formatting by markdown
@@ -61,9 +74,11 @@ methodDirectory.forEach(async (directory) => {
     .build([directory])
     .then(documentation.formats.md)
     .then((output) => {
+      console.log('🍉 ~ .then ~ output:', output);
       console.info('Writing to:', OUTPUT_FILE_PATH);
       // replace ##title with #title for docusaurus
       const strippedOutput = output.replace(/^##\n?/m, '#');
+      // console.log('🍉 ~ .then ~ strippedOutput:', strippedOutput);
 
       // // output is a string of Markdown data
       fs.writeFileSync(OUTPUT_FILE_PATH, strippedOutput);
