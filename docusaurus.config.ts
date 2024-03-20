fbce1b8fd04f1c6e0e17169babbe0cf9087ac606
Devlin Pajaron <iamdevlinph@gmail.com>
2024-03-21 02:53:39 +0800

    Update org and project name in docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'iamdevlinph', // Usually your GitHub org/user name.
+  projectName: 'common-utils-pkg', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
