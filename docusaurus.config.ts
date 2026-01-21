2289c3c781c09742488ff231ac07c884c44b8af2
Devlin Pajaron <iamdevlinph@gmail.com>
2026-01-22 04:06:26 +0800

    Hide footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,7 +35,7 @@ const config: Config = {
       {
         docs: {
           path: 'docs-md',
-          routeBasePath: '/',
+          routeBasePath: '/api',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -114,51 +114,51 @@ const config: Config = {
         },
       ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        // {
-        //   title: 'Docs',
-        //   items: [
-        //     {
-        //       label: 'Tutorial',
-        //       to: '/docs-md/getting-started',
-        //     },
-        //   ],
-        // },
-        // {
-        //   title: 'Community',
-        //   items: [
-        //     {
-        //       label: 'Stack Overflow',
-        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //     },
-        //     {
-        //       label: 'Discord',
-        //       href: 'https://discordapp.com/invite/docusaurus',
-        //     },
-        //     {
-        //       label: 'Twitter',
-        //       href: 'https://twitter.com/docusaurus',
-        //     },
-        //   ],
-        // },
-        // {
-        //   title: 'More',
-        //   items: [
-        //     {
-        //       label: 'Blog',
-        //       to: '/blog',
-        //     },
-        //     {
-        //       label: 'GitHub',
-        //       href: 'https://github.com/facebook/docusaurus',
-        //     },
-        //   ],
-        // },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
+    // footer: {
+    //   style: 'dark',
+    //   links: [
+    //     // {
+    //     //   title: 'Docs',
+    //     //   items: [
+    //     //     {
+    //     //       label: 'Tutorial',
+    //     //       to: '/docs-md/getting-started',
+    //     //     },
+    //     //   ],
+    //     // },
+    //     // {
+    //     //   title: 'Community',
+    //     //   items: [
+    //     //     {
+    //     //       label: 'Stack Overflow',
+    //     //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+    //     //     },
+    //     //     {
+    //     //       label: 'Discord',
+    //     //       href: 'https://discordapp.com/invite/docusaurus',
+    //     //     },
+    //     //     {
+    //     //       label: 'Twitter',
+    //     //       href: 'https://twitter.com/docusaurus',
+    //     //     },
+    //     //   ],
+    //     // },
+    //     // {
+    //     //   title: 'More',
+    //     //   items: [
+    //     //     {
+    //     //       label: 'Blog',
+    //     //       to: '/blog',
+    //     //     },
+    //     //     {
+    //     //       label: 'GitHub',
+    //     //       href: 'https://github.com/facebook/docusaurus',
+    //     //     },
+    //     //   ],
+    //     // },
+    //   ],
+    //   // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    // },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
