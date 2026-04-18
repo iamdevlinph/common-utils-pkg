186b743f138d319a678bd18688cdfa7d2033f9bc
Devlin Pajaron <iamdevlinph@gmail.com>
2026-04-18 20:28:58 +0800

    Silence dts error from tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
+  // dts: true, // Generate declaration file (.d.ts)
+  dts: {
+    compilerOptions: {
+      ignoreDeprecations: '6.0',
+    },
+  },
   splitting: false,
   sourcemap: true,
   clean: true,
