11e5cd5daffc0f937a929e0590172c19b2d31d29
Devlin Pajaron <iamdevlinph@gmail.com>
2026-06-24 17:32:17 +0800

    Just fix build error (#247)

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: {
+    compilerOptions: {
+      ignoreDeprecations: '6.0',
+    },
+  },
   sourcemap: true,
   clean: true,
 });
